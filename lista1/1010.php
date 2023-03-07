a6608a6c3b9f3d2eec53ee6d07c3c5bdd938c423
Larissa-Alves-de-Oliveira <alvesoliveira977@gmail.com>
2023-03-07 10:38:19 -0300

    po

--- lista1/1010.php
+++ lista1/1010.php
@@ -1,12 +1,7 @@
 <?php 
-    $nome = readline();
-    $salaf = readline();
-    $vendas = readline();
-
-    $comi = $vendas * 0.15;
-    $final = $salaf + $comi;
-    $final = number_format($final, 2, ".", "");
-
-    echo "TOTAL = $final\n";
-
+    $valor1 = explode(" ", fgets(STDIN));
+    $valor2 = explode(" ", fgets(STDIN));
+    $calc = ($valor1[2] * $valor1[1]) + ($valor2[2] * $valor2[1]);
+    $calc = number_format($calc, 2, ".", "");
+    echo "VALOR A PAGAR: R$ $calc\n";
 ?>
--- lista1/1011.php
+++ lista1/1011.php
@@ -1,23 +1,7 @@
 <?php
-    //linha 1 - 12 1 5.30
-    //linha 2 - 16 2 5.10
-    $linha1 = readline();
-    $linha2 = readline();
-
-    $val1 = explode('',$linha1);
-    $qtd1 = intval($val1[1]);
-    $vlr1 = floatval($val1[2]);
-
-    $val2 = explode('',$linha2);
-    $qtd2 = intval($val1[1]);
-    $vlr2 = floatval($val1[2]);
-
-    $total = ($qtd1 * $vlr1) + ($qtd2 * $vlr2);
-    $total = number_format($total, 2, ".", "");
-
-    echo "VALOR A PAGAR: RS $total\n";
-
-
-
-
+    $valor1 = explode(" ", fgets(STDIN));
+    $valor2 = explode(" ", fgets(STDIN));
+    $calc = ($valor1[2] * $valor1[1]) + ($valor2[2] * $valor2[1]);
+    $calc = number_format($calc, 2, ".", "");
+    echo "VALOR A PAGAR: R$ $calc\n"
 ?>
