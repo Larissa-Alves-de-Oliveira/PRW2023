8ef2cb4bd4350b1442473019e1eca8ca18e4436a
Larissa-Alves-de-Oliveira <alvesoliveira977@gmail.com>
2023-04-25 11:21:47 -0300

    formulario-lista

--- sexto-formulario-lista/ex6.php
+++ sexto-formulario-lista/ex6.php
@@ -1,3 +1,20 @@
 <?php
+    $nome = $_POST['nome'];
+    $n1 = $_POST['n1'];
+    $n2 = $_POST['n2'];
+    $fq = $_POST['frequencia'];
 
+    $media = ($n1 + $n2) / 2;
+
+    if($media > 5 && $fq > 75){
+        echo "Aluno aprovado";
+    }
+
+    if( $fq > 75 && $media < 5){
+        echo "Reprovado por nota";
+    }
+
+    if($media > 5 &&  $fq < 75){
+        echo "Reprovado por falta";
+    }
 ?>
