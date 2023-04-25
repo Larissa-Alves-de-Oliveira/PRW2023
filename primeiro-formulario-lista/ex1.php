d11037f544fa3d2f97eae33f539464de0fbfacd9
Larissa-Alves-de-Oliveira <alvesoliveira977@gmail.com>
2023-04-25 08:54:05 -0300

    Lista-formulário

--- primeiro-formulario-lista/ex1.php
+++ primeiro-formulario-lista/ex1.php
@@ -1,4 +1,26 @@
 <?php
     $n1 = $_POST ['primeiro'];
     $n2 = $_POST ['segundo'];
+
+    $val = $_POST['escolha'];
+
+    if($val == 1){
+        $calc = $n1 + $n2;
+        echo $calc;
+    }
+
+    if($val == 2){
+        $calc = $n1 - $n2;
+        echo $calc;
+    }
+
+    if($val == 3){
+        $calc = $n1 / $n2;
+        echo $calc;
+    }
+
+    if($val == 4){
+        $calc = $n1 * $n2;
+        echo $calc;
+    }
 ?>
